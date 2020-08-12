e156db821cca947fe09c190df22e99456a9a7a03
tailonls <tailonlimas@gmail.com>
2020-08-11 21:15:04 -0300

    criando pagin ahtml

--- core/models.py
+++ core/models.py
@@ -12,3 +12,10 @@ class Evento(models.Model):
     # Definindo nome da tabela:
     class Meta:
         db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
+    def get_data_evento_formatada(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M h')
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from core.models import Evento
+
+
 # Create your views here.
+
+def lista_eventos(request):
+    usuario = request.user
+    eventos = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos': eventos}
+    return render(request, 'agenda.html', dados)
